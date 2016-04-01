e7ccff42bf7dc937be87efff1729125b82aec22a
jaime irazabal <jaimeirazabal1@gmail.com>
2016-04-01 16:39:24 -0430

    hacer bien el autocomplete

--- default/app/controllers/index_controller.php
+++ default/app/controllers/index_controller.php
@@ -11,5 +11,9 @@ class IndexController extends AppController
     {
         
     }
+    public function buscar_por_codigo_producto($codigo){
+    	View::select(null,"json");
+    	$this->data = Load::model("stockmaster")->find("conditions: stockid like '%".$codigo."%'");
+    }
 
 }
