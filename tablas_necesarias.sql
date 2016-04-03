b53224ced2baef6223b91a1ad13bba0aaa47b09d
Jaime Irazabal <jaimeirazabal1@gmail.com>
2016-04-03 08:11:56 +0200

    agregando las tablas necesarias

---
+++ tablas_necesarias.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE `estado` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `estado`
+--
+
+INSERT INTO `estado` (`id`, `nombre`) VALUES
+(1, 'Estado 1'),
+(2, 'Estado 2'),
+(3, 'Estado 3');
+
+
+CREATE TABLE `productos_espera` (
+  `id` int(11) NOT NULL,
+  `producto` varchar(10) NOT NULL,
+  `nombre_producto` varchar(200) NOT NULL,
+  `sucursal` varchar(10) NOT NULL,
+  `cliente` varchar(200) NOT NULL,
+  `telefono` varchar(100) NOT NULL,
+  `estado_id` int(11) DEFAULT NULL,
+  `fecha` datetime NOT NULL,
+  `observacion` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
